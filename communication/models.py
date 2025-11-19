2dcd3ee127c5942ae74cbbc8cbc8230b00d74ff2
shukuverity <167431273+shukuverity@users.noreply.github.com>
2025-11-19 11:20:17 +0300

    Initial setup

--- communication/models.py
+++ communication/models.py
@@ -1,16 +1,3 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Ticket(models.Model):
-
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-    
 class Comment(models.Model):
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name= 'comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commented')
